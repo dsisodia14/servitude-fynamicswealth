77ce28eea73c3aa36d2f435fd6c872887b3c57aa
vaibhavworldwide <vaibhavsingh1008@gmail.com>
2021-12-02 10:54:05 +0530

    Fynamics Wealth

---
+++ dematAccount.php
@@ -0,0 +1,32 @@
+<?php include "includes/header.php"?>
+
+<div class=" section-title top">
+<h3>Demat<span> Account </span> </h3> <br></div>
+
+<div class="section-title" >
+<h3> 
+
+What is a  <span>Demat Account?</span>?</h3>
+</div>
+
+<div class="container size bottom">
+
+Initially, stocks and shares used to be exchanged via physical receipts called certificates. However, this resulted in lengthy paperwork and took up a lot of time. To counter this and to take advantage of an electronic trading platform which was gaining traction in West and the Asian Markets, the process of dematerialisation (demat) of shares was initiated in 1996. Physical share certificates were converted into electronic form securities of equivalent number and price and were credited to the investor’s demat account. Thus, the advent of trading commenced this way.
+<br>
+In a more simplistic approach, demat accounts allow the investor to buy and sell as well as transact not only shares and stocks but other products conveniently without the need of any sort of paperwork.
+​<br>
+<div class=" section-title ">
+<h3>
+<br>​
+
+Why you need to<span>  Open Demat </span>Account?
+</h3> <br></div>
+
+​
+Such accounts are of crucial importance today because, the entire financial platform of investing, trading and maintaining have become digitised. Hence, to enable the user with a seamless and straightforward experience, demat accounts are the necessity of the day. These accounts are essential to trade in India’s stock exchanges. Although SEBI – Securities and Exchange Board of India, has permitted traded up to 500 shares to be settled in the physical form, it is not a preferable option these days since it causes excessive inconvenience of maintenance and tracking.
+<br>
+This account retains certificates of financial instruments such as shares, bonds, government securities, etc. Thus, the dematerialisation account aids the investor to maintain their investments systematically as well as concedes an easy movement to buy and sell any products they desire.
+​</div>
+
+
+<?php include "includes/footer.php"?>
---
+++ ipo.php
@@ -0,0 +1,95 @@
+<?php include "includes/header.php"?>
+
+<div class=" section-title top">
+<h3><span>IPO</span> </h3> <br></div>
+
+
+<div class="container size">
+
+<div class="section-title" >
+    <b> When a privately held business collects money from the public and in turn gives them  shares in their company. The shares are then traded for the first time in the stock market. The process is called Initial Public Offering.</b>
+
+<br>
+    <h3>Why companies<span>  go public</span>?</h3>
+</div>
+
+Companies go public to raise money. It gives them financial capital, that they can use to clear off debts, improve the infrastructure, invest in research and development of new products, introduce new products and so on. Apart from that, the increased financial scrutiny during the process of going public will get them better debt rates when they are issuing it.
+<br>
+And if the company’s stocks are in demand, there is always scope for mergers and acquisitions. The terms of negotiations can be in stocks. The demand also attracts the company’s top talents as the company can offer stock options as reimbursement. The company gets credibility and visibility after it gets listed on the stock exchange.
+
+​
+<div class="section-title" >
+<h3>Who are underwriters and <span>  what do they do?</span>?</h3>
+</div>
+Investment banks send a few representatives to the company who want to sell their shares to the public in the market for the very first time. They work out the complete process for them, to ensure successful entry into the market. The amount of risk the underwriter takes depends on how he is compensated. There are two ways of underwriting.
+​<br>
+
+<b> -> Bought deal –  </b>Here the underwriters buy all IPOs from the Company and they resell it to the investing public. The compensation is the difference in the selling and buying price. The entire risk of the IPO listing outcome is borne by the underwriters.<br>
+<b> --> </b>Death benefits are generally income-tax-free to the beneficiary.
+<br>
+<b> --> </b>Death benefits may be estate-tax free if the policy is owned properly.
+<br>
+<b> --> </b>Cash values grow tax deferred during the insured’s lifetime.
+<br>
+<b> --> </b>Policy loans are income tax free.
+<br>
+<b> --> </b>A life insurance policy may be exchanged for another life insurance policy (or for an annuity) without incurring current taxation.
+
+​
+<br>
+<b> Note: All of the above statements are generally true; however the tax benefits of life insurance have certain limitations which under the wrong set of circumstances can cause the tax benefits mentioned to be lost. Please discuss with your insurance and tax advisor.</b>
+<br>
+
+
+
+<b> -> </b>Many life insurance policies are exceptionally flexible in terms of adjusting to the policyholder’s needs. The death benefit may be decreased at any time and the premiums may be easily reduced, skipped or increased.
+<br>
+<b> -> </b>A cash value life insurance policy may be thought of as a tax-favored repository of easily accessible funds if the need arises; yet, the assets backing these funds are generally held in longer-term investments, thereby earning a higher return.
+<br>
+<br>
+<b>Disadvantages of Life Insurance
+</b> <br><br>
+<b> -> </b>Policyholders forego some current expenditure to pay policy premiums. Moreover, life insurance is typically purchased for the benefit of others and usually only indirectly for the insured person.<br>
+<b> -> </b>Cash surrender values are usually less than the premiums paid in the first several policy years and sometimes a policyowner may not recover the premiums paid if the policy is surrendered.<br>
+<b> -> </b>The life insurance purchase decision and the positioning of the life insurance can be complex especially if the insurance is for estate planning, business situations or complex family situations.
+​<br>
+<b> -> </b>The life insurance acquisition process can be annoying and perplexing (e.g. Is the life insurance agent trustworthy? Is this the right product and carrier? How can medical underwriting be streamlined?).
+<br>​
+</div>
+
+<div class="container bottom">
+​<div class="section-title">
+<h3>
+Types of<span> Life Insurance</span><br></h3>
+</div>
+​
+
+There are certain basic forms of life insurance. The different types of life insurance policies include:
+​​<br><b>
+1 Term Life Insurance</b>
+<br>
+Term insurance is a life insurance product offered by an insurance company which offers financial coverage to the policy holder for a specific time period. <br><b>
+2 Whole Life Policy</b>
+<br>
+The policyholder pays regular premiums until his death, upon which the corpus is paid out to the family. 
+
+​
+<br><b>
+3 Endowment Plans</b>
+<br>
+Endowment plans pay out the sum assured under both scenarios - death and survival 
+<br><b>
+4 Unit Linked Insurance Plans</b><br>
+ULIP is a life insurance product, which provides risk cover for the policy holder along with investment options to invest in any number of qualified investments. 
+<br><b>
+5 Money Back Policy</b>
+<br>
+Money back plan is a life insurance product as well as an investment plan which provides life insurance cover against death of the policy holder along with periodic returns as a percentage of sum assured.
+
+​
+<br>
+
+<br>
+
+</div>
+<?php include "includes/footer.php"?>
